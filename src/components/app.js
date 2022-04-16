01d3d485096e1f48379844740b0b44640433e673
stravse <stravse@gmail.com>
2022-04-16 22:32:16 +0800

    added some more logic

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 function App(){
     const [rest, setRest] = useState(5);
     const [session, setSession] = useState(25);
+    useEffect(()=>{
+        setTotalSec(session*60);
+    },[session]);
+
+    const [totalSec,setTotalSec] = useState(session*60);
+    useEffect(() => {console.log(totalSec)}
+    ,[totalSec]);
+
+    let minute = Math.floor(totalSec/ 60);
+    let sec = totalSec % 60;
+
+    const [start, setStart] = useState(false);
+    useEffect(() => {
+        let interval = null;
+        if(start){
+            interval = setInterval(()=> {
+                if (totalSec === 0) {
+                    setStart(false);
+                }else {
+                    setTotalSec(prevSec => prevSec - 1)
+                }
+            }, 1000);
+        } else{
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+    },[start, totalSec]);
+
+
+    
 
     function addSession(){
         setSession(prev => prev + 1);
@@ -23,33 +54,48 @@ function App(){
         }
         setRest(prev=> prev - 1);
     }
+    function startTimer(){
+        setStart(true);
+    }
+    function stopTimer(){
+        setStart(false);
+    }
+    function resetTimer(){
+        setStart(false);
+        setTotalSec(session*60);
+    }
 
     return(
         <div className="true-container">
             <div className="main-container">
+
                 <div className="container">
                     <div>{rest}</div>
                     <div className="button-container">
-                        <button onClick={addRest}>add</button>
-                        <button onClick={minRest}>reduce</button>
+                        <button disabled={start? true: false} onClick={addRest}>add</button>
+                        <button disabled={start? true: false} onClick={minRest}>reduce</button>
                     </div>
                 </div>
+
                 <div className="container">
                     <div>{session}</div>
                     <div className="button-container">
-                        <button onClick={addSession}>add</button>
-                        <button onClick={minSession}>reduce</button>
+                        <button disabled={start? true: false} onClick={addSession}>add</button>
+                        <button disabled={start? true: false} onClick={minSession}>reduce</button>
                     </div>
                 </div>
+
             </div>
 
             <div className="timer-container">
-                <div className="display-timer">{session}</div>
+
+                <div className="display-timer">{minute.toString().length===1? "0"+minute.toString(): minute}:{sec.toString().length === 1? "0"+sec.toString(): sec}</div>
                 <div className="button-container">
-                    <button>play</button>
-                    <button>pause</button>
-                    <button>reset</button>
+                    <button onClick={startTimer}>play</button>
+                    <button onClick={stopTimer}>pause</button>
+                    <button onClick={resetTimer}>reset</button>
                 </div>
+                
             </div>
 
         </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/app";
 import "./sass/index.scss";
 
-const root = document.getElementById("root");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(<App />, root);
+root.render(<App />);
 
